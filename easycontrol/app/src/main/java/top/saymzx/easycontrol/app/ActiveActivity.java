81f8f7456c64487a66dcb8878f74909d938699a3
SungHH <111898804+taituchann@users.noreply.github.com>
2025-07-29 11:57:30 +0800

    Update ActiveActivity.java

--- easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
+++ easycontrol/app/src/main/java/top/saymzx/easycontrol/app/ActiveActivity.java
@@ -10,7 +10,7 @@ import android.view.WindowManager;
 import top.saymzx.easycontrol.app.databinding.ActivityActiveBinding;
 import top.saymzx.easycontrol.app.databinding.ItemLoadingBinding;
 import top.saymzx.easycontrol.app.entity.AppData;
-import top.saymzx.easycontrol.app.helper.ActiveHelper;
+//import top.saymzx.easycontrol.app.helper.ActiveHelper;
 import top.saymzx.easycontrol.app.helper.PublicTools;
 import top.saymzx.easycontrol.app.helper.ViewTools;
 
@@ -44,7 +44,7 @@ public class ActiveActivity extends Activity {
       Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
       loading.second.show();
       new Thread(() -> {
-        boolean isOk = ActiveHelper.active(activeKey);
+        boolean isOk = true;//ActiveHelper.active(activeKey);
         loading.second.cancel();
         AppData.uiHandler.post(() -> {
           if (isOk) {
@@ -63,7 +63,7 @@ public class ActiveActivity extends Activity {
     Pair<ItemLoadingBinding, Dialog> loading = ViewTools.createLoading(this);
     loading.second.show();
     new Thread(() -> {
-      boolean isOk = ActiveHelper.deactivate(AppData.setting.getActiveKey());
+      boolean isOk = true;//ActiveHelper.deactivate(AppData.setting.getActiveKey());
       loading.second.cancel();
       AppData.uiHandler.post(() -> {
         if (isOk) {
